8a79217de33d3d079e113e326f997bf2fecf7757
saqib hussain <hussainsakib44@gmail.com>
2022-03-04 08:37:18 +0530

    Update project.js

--- Pong Game/project.js
+++ Pong Game/project.js
@@ -1,1 +1,7 @@
-//
+let container = document.querySelector('.container');
+let me = document.querySelector('.me');
+
+container.addEventListener('mousemove', e => {
+  let x = e.offsetX;
+  me.style.marginLeft = '' + x + 'px';
+});
